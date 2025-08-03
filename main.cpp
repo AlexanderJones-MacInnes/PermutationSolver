296d1748e8de59347dae6193d309a60ba9179cb3
SheriffTubbyNips <205615287+SheriffTubbyNips@users.noreply.github.com>
2025-08-03 10:18:14 -0300

    Added ID system for set-members.
    Changed the system to assign each set-member an integer ID, which is now used instead of the member's value for the process of finding all the possible permutations. This new system avoids conflicts between repeating values in the set and the check to avoid placing the same member in a permutation more than once.

--- main.cpp
+++ main.cpp
@@ -21,8 +21,42 @@ struct PermutationSolver
         }
     };
 
-    vector<vector<string>> permutations;
+    list<string> permutations; //Stores permutation results by their member value. (Converted from idOrder)
+    list<Member> members; //Stores set members.
 
+    void resetData()
+    {
+        members.clear();
+        permutations.clear();
+    }
+
+    void inputSet(string text) //Input set of members as string
+    {
+        for(char c:text)
+        {
+            string s;
+            s += c;
+            members.push_back({s});
+        }
+    }
+
+    void inputSet(list<string> text) //Input set of members as list of strings
+    {
+        for(string s:text)
+        {
+            members.push_back({s});
+        }
+    }
+
+    void printResults()
+    {
+        for(string s:permutations)
+        {
+            cout << s << '\n';
+        }
+    }
+
+    //Calculates factorial. (Used during calculation of permutations)
     int Factorial(int n)
     {   int f = n;
         if(f == 0)
@@ -39,37 +73,54 @@ struct PermutationSolver
         return f;
     }
 
-    void Solve(list<string> members)
+    void Solve()
     {
+        //Get total amount of members that were input.
+        int totalMembers = members.size();
 
-        int perTotal = Factorial(members.size());
-        permutations.resize(perTotal);
-        list<string> memberQueue;
-        //Iterate through row selection of columns in permutation list
-        for(int cycle = 0; cycle < members.size(); cycle++)
+        //Find total amount of possible permutations via the factorial of the total count of members.
+        int perTotal = Factorial(totalMembers);
+
+        //Create a vector to handle storing combinations of member ID's for calculation of permutations.
+        vector<vector<int>> idOrder;
+        //Resize permutation vector to contain total amount of possible permutations.
+        idOrder.resize(perTotal);
+
+        //Create a list to act as a queue for handling emplacment of members in each permutation.
+        list<int> idQueue;
+        //Create a second list of all ID's to act as a template for resetting the idQueue later during calculation.
+        list<int> ids;
+        for(Member m:members)
+        {
+            ids.push_back(m.ID);
+        }
+
+        //Loop through currently selected row-space for member-emplacement.
+        for(int cycle = 0; cycle < totalMembers; cycle++)
         {
             /*
-            To know how many times to place a member in each row you can treat
-            each row as a smaller set read from the current row to the right
+            To know how many times to place a member you can treat
+            each row as a smaller set, read from the current column to the
+            right, ignoring the already emplaced members on the left.
             */
-            int empTotal = Factorial( (members.size() - cycle) -1 );
+            int empTotal = Factorial( (totalMembers - cycle) -1); // -1 for vector indexing purposes.
 
-            //Reset queue for each row-cycle otherwise ya get problems.
-            memberQueue = members;
+            //Reset queue for each cycle otherwise ya get problems.
+            idQueue = ids;
 
-            //Iterate through each permutation in the list at the row-number equal to value of cycle
+            //Iterate through each column in the permutation vector.
             for(int index = 0; index < perTotal; index++)
             {
                 //When you have emplaced a single member for it's maximum repeats...
                 if(index % empTotal == 0 && index != 0)
                 {
                     //...Pop queue to next member
-                    memberQueue.pop_front();
+                    idQueue.pop_front();
                     //If you have emplaced the entire queue...
-                    if(memberQueue.empty())
+                    if(idQueue.empty())
                     {
                         //...refresh the queue
-                        memberQueue = members;
+                        idQueue = ids;
                     }
                 }
 
@@ -78,16 +129,33 @@ struct PermutationSolver
                 the queue to avoid repeating instances. This should be done after the
                 prior IF-statement to avoid emptying the queue before emplacemnt.
                 */
-                for(string s:permutations.at(index))
+                for(int id:idOrder.at(index))
                 {
-                    memberQueue.remove(s);
+                    idQueue.remove(id);
                 }
 
-                //Push data to permutation list.
-                permutations.at(index).push_back(memberQueue.front());
+                //Push data to idOrder list.
+                idOrder.at(index).push_back(idQueue.front());
             }
         }
 
+        //Convert idOrder list to readable results and store it in permutations list.
+        for(vector<int> lineRead:idOrder) //Iterate through each permutation in idOrder
+        {
+            string temp;
+            for(int idRead:lineRead) //Iterate through each member ID in the current permutation
+            {
+                for(Member m:members) //Iterate through members in list
+                {
+                    if(m.ID == idRead) //Find the member matching the currently read ID
+                    {
+                        temp += m.text; //Push that members value to a temp string
+                        break; //Found member so stop checking member list and loop to next ID.
+                    }
+                }
+            }
+            permutations.push_back(temp); //Once entire permutation has been read push string.
+        }
     }
 };
 int PermutationSolver::Member::nextID = 0; //Initialize default value for member struct ID
@@ -95,18 +163,7 @@ int PermutationSolver::Member::nextID = 0; //Initialize default value for member
 int main()
 {
     PermutationSolver p;
-    list<string> test = {"a","b","c"};
-    p.Solve(test);
-    for(vector<string> v:p.permutations)
-    {
-        for(string s:v)
-        {
-            cout << s << ',';
-        }
-        cout << '\n';
-    }
-
-
-
-
+    p.inputSet("abcdef");
+    p.Solve();
+    p.printResults();
 }
